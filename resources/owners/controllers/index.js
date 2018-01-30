74a44639d7dec738e7f65fa0f27b73eb511e1ea9
Arun Sood <arsood@gmail.com>
2018-01-30 07:37:22 -0800

    Perform join lookup for owners and pets

--- resources/owners/controllers/index.js
+++ resources/owners/controllers/index.js
@@ -8,7 +8,19 @@ module.exports = (req, res) => {
   // Aggregate them using the $lookup operator to join their associated pets
   // Call the method below, passing in the owners array:
   MongoClient.connect(mongoUrl, (err, db) => {
-    db.collection("owners").find({}).toArray((err, owners) => {
+    db
+    .collection("owners")
+    .aggregate([
+      {
+        $lookup: {
+          from: "pets",
+          localField: "_id",
+          foreignField: "ownerId",
+          as: "pets"
+        }
+      }
+    ])
+    .toArray((err, owners) => {
       db.close();
       // Render index template with owner records from Mongo
       res.render("index", {
