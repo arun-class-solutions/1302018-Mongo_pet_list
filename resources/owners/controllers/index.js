aedacdc6e4980c0da924410e8db549df21d48da3
Arun Sood <arsood@gmail.com>
2018-01-30 06:48:48 -0800

    Show all owners

--- resources/owners/controllers/index.js
+++ resources/owners/controllers/index.js
@@ -7,6 +7,14 @@ module.exports = (req, res) => {
   // Find all owners from the "owners" collection here
   // Aggregate them using the $lookup operator to join their associated pets
   // Call the method below, passing in the owners array:
+  MongoClient.connect(mongoUrl, (err, db) => {
+    db.collection("owners").find({}).toArray((err, owners) => {
+      // Render index template with owner records from Mongo
+      res.render("index", {
+        owners: owners
+      });
+    });
+  });
 
   // res.render("index", {
   //   owners: ownersArray
